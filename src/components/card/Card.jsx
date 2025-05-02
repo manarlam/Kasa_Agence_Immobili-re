15730f495438eccf1f1b8f8551bc307272ef598f
Manar <75319989+manarlam@users.noreply.github.com>
2025-05-02 10:25:01 +0200

    fix_home_gallery

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({ image, title }) {
     return (
         <div className="card">
-        <img className="card_img" src={image} alt="card" />
+        <img className="card_img" src={image} alt={title} />
         <h2 className="card_title">{title}</h2>
         </div>
     );
