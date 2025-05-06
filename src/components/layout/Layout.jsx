5057a92bb91e0fd558986fb41844eceebdb81b02
Manar <75319989+manarlam@users.noreply.github.com>
2025-05-06 18:20:56 +0200

    fix_scss_header+cards
    Correction du scss du header et des cards

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,10 +5,12 @@ import { Outlet } from 'react-router-dom';
 function Layout() {
   return (
     <>
+    <div className="layout">
       <Header />
       <main>
         <Outlet />
       </main>
+    </div>
     </>
   );
 }
